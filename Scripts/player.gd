3a414ca45b2cb59243794dd2f5084e656c1c5cd6
Denyol18 <sprokdaniel@gmail.com>
2025-03-03 20:36:52 +0100

    player created, testing environment

---
+++ Scripts/player.gd
@@ -0,0 +1,46 @@
+extends CharacterBody3D
+
+
+const JUMP_VELOCITY = 4.8
+
+var speed = 8.0
+var gravity = 9.8
+var sensitivity = 0.003
+
+@onready var head = $Head
+@onready var camera = $Head/Camera3D
+
+
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		head.rotate_y(-event.relative.x * sensitivity)
+		camera.rotate_x(-event.relative.y * sensitivity)
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-60), deg_to_rad(80))
+
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	
+	var input_dir := Input.get_vector("left", "right", "forward", "backwards")
+	var direction = (head.transform.basis * transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	if is_on_floor():
+		if direction:
+			velocity.x = direction.x * speed
+			velocity.z = direction.z * speed
+		else:
+			velocity.x = lerp(velocity.x, direction.x * speed, delta * 7.0)
+			velocity.z = lerp(velocity.z, direction.z * speed, delta * 7.0)
+	else:
+		velocity.x = lerp(velocity.x, direction.x * speed, delta * 3.0)
+		velocity.z = lerp(velocity.z, direction.z * speed, delta * 3.0)
+
+	move_and_slide()
